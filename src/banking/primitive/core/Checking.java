f26e600e8841661521eea47cef521a1ebd7348de
quinten <qbecker@ecn-10.mobile.asu.edu>
2016-02-21 20:06:42 -0700

    Captialized SerialVersionUID as described in issue #5

--- src/banking/primitive/core/Checking.java
+++ src/banking/primitive/core/Checking.java
@@ -1,8 +1,8 @@
 package banking.primitive.core;
 
 public class Checking extends Account {
-
-	private static final long serialVersionUID = 11L;
+	//qbecker issue #5 fix
+	private static final long SerialVersionUID = 11L;
 	private int numWithdraws = 0;
 	
 	private Checking(String name) {
